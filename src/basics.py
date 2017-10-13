f2803db96b940a5b0de476296c8489e1f5431252
Ji Won Yeom <jwyeom63@gmail.com>
2017-10-13 17:52:41 +0900

    caculate fields that is more likely to stay

--- src/basics.py
+++ src/basics.py
@@ -11,5 +11,28 @@ df = pd.read_csv('HR_comma_sep.csv')
 
 #attempting to sort
 df = df.sort_values(['satisfaction_level','time_spend_company'], ascending=[0,1])
-print df[['satisfaction_level','time_spend_company']]
+#print(dfAcc[['satisfaction_level','time_spend_company','sales']])
 
+#by job field
+#total
+accTotal = len(df.loc[df["sales"] == 'accounting'].index)
+itTotal =len(df.loc[(df["sales"] == 'IT') | (df["sales"] == 'technical')].index)
+salesTotal =  len(df.loc[df["sales"] == 'sales'].index)
+
+dfAcc = df.loc[df["sales"] == 'accounting']
+dfTech = df.loc[(df["sales"] == 'IT') | (df["sales"] == 'technical')]
+accountLong = dfAcc.loc[df["time_spend_company"] > 8]
+ITLong = dfTech.loc[df["time_spend_company"] > 8]
+salesLong = df.loc[(df["sales"] == 'sales') & (df["time_spend_company"] > 8)]
+
+accLongCnt = len(accountLong.index)
+ITLongCnt = len(ITLong.index)
+salesLongCnt = len(salesLong.index)
+
+#relative stay rates
+print("Account")
+print(accLongCnt/accTotal * 100)
+print("IT/Tech")
+print(ITLongCnt/itTotal * 100)
+print("sales")
+print(salesLongCnt/salesTotal * 100)
